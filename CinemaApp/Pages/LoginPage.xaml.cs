5b7c79005f6303588f28ac2fcc00d9f583ba951c
maniac-666 <ekate@LAPTOP-Q68JF043>
2026-03-30 23:20:44 +0300

    Исправление.

--- CinemaApp/Pages/LoginPage.xaml.cs
+++ CinemaApp/Pages/LoginPage.xaml.cs
@@ -13,29 +13,28 @@ namespace CinemaApp.Pages
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
-            Auth(TbLogin.Text, PbPassword.Password);
+            bool result = Auth(TbLogin.Text, PbPassword.Password);
+
+            if (result)
+            {
+                MessageBox.Show("Добро пожаловать, " + Core.CurrentUser.FullName);
+                NavigationService.Navigate(new MainPage());
+            }
         }
 
         public bool Auth(string login, string password)
         {
             if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
-            {
-                MessageBox.Show("Заполните все поля!");
                 return false;
-            }
 
             using (var db = new CinemaDBEntities())
             {
                 var user = db.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
 
                 if (user == null)
-                {
-                    MessageBox.Show("Неверный логин или пароль!");
                     return false;
-                }
 
                 Core.CurrentUser = user;
-                MessageBox.Show("Добро пожаловать, " + user.FullName);
                 return true;
             }
         }
